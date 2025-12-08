bfb1116f2eaea63471398cbfb4d753491d2ac629
dredyi <andsar03@mail.ru>
2025-12-08 22:18:19 +0300

    Update template_p01.py
    Сохраняю для ответа на задание

--- Deep Learning/template_p01.py
+++ Deep Learning/template_p01.py
@@ -18,12 +18,19 @@ def multiplicative_attention(decoder_hidden_state, encoder_hidden_states, W_mult
     decoder_hidden_state: np.array of shape (n_features_dec, 1)
     encoder_hidden_states: np.array of shape (n_features_enc, n_states)
     W_mult: np.array of shape (n_features_dec, n_features_enc)
-    
+
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
     '''
-    # your code here
+
+    transformed = decoder_hidden_state.T @ W_mult
+
+    attention_scores = transformed @ encoder_hidden_states
+
+    weights = softmax(attention_scores)
     
+    attention_vector = weights @ encoder_hidden_states.T
+
     return attention_vector
 
 def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add_enc, W_add_dec):
@@ -33,10 +40,18 @@ def additive_attention(decoder_hidden_state, encoder_hidden_states, v_add, W_add
     v_add: np.array of shape (n_features_int, 1)
     W_add_enc: np.array of shape (n_features_int, n_features_enc)
     W_add_dec: np.array of shape (n_features_int, n_features_dec)
-    
+
     return: np.array of shape (n_features_enc, 1)
         Final attention vector
     '''
-    # your code here
-    
+    enc_proj = W_add_enc @ encoder_hidden_states
+
+    dec_proj = W_add_dec @ decoder_hidden_state
+
+    combined = enc_proj + dec_proj
+    tanh_out = np.tanh(combined)
+    scores = v_add.T @ tanh_out
+    weights = softmax(scores)
+    context = weights @ encoder_hidden_states.T
+    attention_vector = context.T
     return attention_vector
